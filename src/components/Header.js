c11f9b62cc5d7159efb75e2394c4719ed540322b
Hilary Lewis <hilarylewis92@gmail.com>
2016-11-29 16:34:15 -0700

    Renders header component to App component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return(
+    <article>
+      <h1>Weather Tracker</h1>
+      <section>
+        <p>
+          Current forecast for props city: props forecast props temp
+        </p>
+        <p>
+          View Extended Forecast
+        </p>
+      </section>
+    </article>
+  )
+}
+
+export default Header
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Header from '../components/Header'
+
 class App extends Component {
   render() {
     return (
       <div>
-        Hey!
+        <Header />
         {this.props.children}
       </div>
     )
