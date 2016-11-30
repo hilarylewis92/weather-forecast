5dc08e63c9d8dbef93c8dabb17222b0d1f5f6bc7
MilkMan90 <mlkaufman747@gmail.com>
2016-11-30 14:20:01 -0700

    combine reducers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { browserHistory } from 'react-router';
 import Routes from './routes';
 import thunk from 'redux-thunk';
-import exampleReducer from './reducers/index';
+import RootReducer from './reducers/RootReducer';
 
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  exampleReducer,
-  applyMiddleware(thunk)
+  RootReducer,
+  {},
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 )
 
 render(
