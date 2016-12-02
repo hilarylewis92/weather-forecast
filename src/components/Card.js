f182e75ef7ba0f53a600c2a3af741e230bf856cc
MilkMan90 <mlkaufman747@gmail.com>
2016-12-01 17:07:42 -0700

    remove cities

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,6 @@ import React from 'react'
 import {Link} from 'react-router'
 
 const Card = ({list, id}) => {
-  console.log(list);
-  console.log(id);
   return(
     <div className="pinned-city-card">
       <Link to="/forecast">View Forecast</Link>
--- src/reducers/PinnedWeatherReducer.js
+++ src/reducers/PinnedWeatherReducer.js
@@ -2,6 +2,9 @@ const PinnedWeatherReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_PINNED_WEATHER':
       return [...state, Object.assign({}, state, action.weatherInfo)]
+    case 'DELETE_PINNED_CITY':
+      console.log(state.slice(action.id+1));
+      return state.slice(action.id+1)
     default:
       return state
   }
