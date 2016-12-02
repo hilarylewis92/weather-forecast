8d3fd2890529d4b640f5114db42758efbcbbcd6d
MilkMan90 <mlkaufman747@gmail.com>
2016-12-01 22:28:43 -0700

    weather-DUCKS!

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,19 @@ import { Link } from 'react-router';
 const Header = ({name, temp, desc}) => {
   return(
     <header>
-      <div className='home-logo' alt="It's a duck!"></div>
-      <h1 className='title'> Weather Dux </h1>
+      <Link className="home-link" to='/'>
+        <div className='home-logo' alt="It's a duck!"></div>
+        <h1 className='title'> Weather Dux </h1>
+      </Link>
       <div className='current-weather'>
-         <span className='current-location'>
-           {name}
-         </span>
          <span className='current-desc'>
            {desc}
          </span>
          <span className='current-temp'>
            {temp}&deg;
+         </span>
+         <span className='current-location'>
+           {name}
          </span>
       </div>
       <Link to="/forecast" className="current-view-extended">
