9e4834c3c8d9fd016f2cb40d33fc02acc1f63194
Dale S Hendrickson <dale@dshendrickson.com>
2016-12-03 13:59:43 -0700

    Fixes eslinting issues on the Card.js file

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import {Link} from 'react-router'
+import { Link } from 'react-router'
 
 
-const Card = ({list, id}) => {
-  return(
+const Card = ({ list, id }) => {
+  return (
     <div className="pinned-city-card">
-        {list[id] ?
-          <div>
-            <h3 className='card-location'>{list[id].current_observation.display_location.full}</h3>
-            <img className='card-img' src={list[id].current_observation.icon_url} />
-            <span className="card-temp">{list[id].current_observation.temp_f}</span>
-            <span className="card-weather">{list[id].current_observation.weather}</span>
-            <Link className="card-link-to-extend" to="/forecast">View Forecast</Link>
-          </div>
-           :
-            <Link className="card-link-to-settings" to="/settings">
-              <div className="card-text-box">Save a New City</div>
-            </Link>
-        }
+      {list[id] ?
+        <div>
+          <h3 className="card-location">{list[id].current_observation.display_location.full}</h3>
+          <img className="card-img" role="presentation" src={list[id].current_observation.icon_url} />
+          <span className="card-temp">{list[id].current_observation.temp_f}</span>
+          <span className="card-weather">{list[id].current_observation.weather}</span>
+          <Link className="card-link-to-extend" to="/forecast">View Forecast</Link>
+        </div>
+         :
+        <Link className="card-link-to-settings" to="/settings">
+          <div className="card-text-box">Save a New City</div>
+        </Link>
+      }
     </div>
   )
 }
