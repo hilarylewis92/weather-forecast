51fc4ffebb3b4b1f52e2c1484557a058f068cd96
Dale S Hendrickson <dale@dshendrickson.com>
2016-12-03 20:34:36 -0700

    Fixes eslinting issues on Settings.js file

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,29 +1,29 @@
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
 import Settings from '../components/Settings'
-import {fetchPinnedLocationWeather} from '../actions/index'
+import { fetchPinnedLocationWeather } from '../actions/index'
 
 const mapStateToProps = (state = []) => {
   if (!state.PinnedWeatherReducer[0]) {
     return {}
   }
- return {
-    cities: state.PinnedWeatherReducer
+  return {
+    cities: state.PinnedWeatherReducer,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSubmit: (zip) => {
-      dispatch (
-        fetchPinnedLocationWeather(zip)
+      dispatch(
+        fetchPinnedLocationWeather(zip),
       )
     },
     deleteCity: (id) => {
       dispatch({
-          type: 'DELETE_PINNED_CITY',
-          id: id
-        })
-    }
+        type: 'DELETE_PINNED_CITY',
+        id,
+      })
+    },
   }
 }
 
