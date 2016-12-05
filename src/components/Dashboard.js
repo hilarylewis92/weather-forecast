02ba858544c490f451266be7fcfbe1fc14981245
Hilary Lewis <hilarylewis92@gmail.com>
2016-12-04 22:23:30 -0700

    Styles dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,10 +4,12 @@ import Card from '../containers/Card'
 
 const Dashboard = () => {
   return (
-    <div className="city-card-container">
-      <Card id={0} />
-      <Card id={1} />
-      <Card id={2} />
+    <div className="dashboard">
+      <div className="city-card-container">
+        <Card id={0} />
+        <Card id={1} />
+        <Card id={2} />
+      </div>
       <Link className="edit-cities-link" to="/settings">
         Edit My Cities
       </Link>
